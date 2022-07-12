0d1d67d662b74a6bb62717781fa8fc3f9ada20ef
Gabriela Santos <gbss.santos@poli.ufrj.br>
2022-07-11 21:53:13 -0300

    metodos

--- conta.py
+++ conta.py
@@ -5,7 +5,24 @@ class Conta:
         self.saldo = saldo
         self.limite = limite
 
+    def extrato(self):
+        print("O titular {} possui saldo de {} reais".format(self.titular,self.saldo))
+
+    def deposita(self,valor):
+        self.saldo+=valor
+
+    def saca(self,valor):
+        self.saldo-=valor
+
 conta = Conta(10, "Gabriela", 70)
 conta2 = Conta(11, "Julia", 90, 2000)
 
 print(conta.saldo)
+
+conta.extrato()
+conta.deposita(10)
+conta.extrato()
+conta.saca(20)
+conta.extrato()
+
+
---
+++ datas.py
@@ -0,0 +1,11 @@
+class Data:
+    def __init__(self, dia, mes, ano):
+        self.dia = dia
+        self.mes = mes
+        self.ano = ano
+    
+    def formatada(self):
+        print("{}/{}/{}".format(self.dia, self.mes, self.ano))
+
+data = Data(17,7,200)
+data.formatada()
