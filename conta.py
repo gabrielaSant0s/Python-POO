5b1b3381289b3e04b181a1a55013c371628fea11
Gabriela Santos <gbss.santos@poli.ufrj.br>
2022-07-12 00:10:16 -0300

    atributos privados e encapsulamento

--- conta.py
+++ conta.py
@@ -1,28 +1,38 @@
 class Conta:
     def __init__(self, numero, titular, saldo, limite=1000):
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print("O titular {} possui saldo de {} reais".format(self.titular,self.saldo))
+        print("O titular {} possui saldo de {} reais".format(
+            self.__titular, self.__saldo))
 
-    def deposita(self,valor):
-        self.saldo+=valor
+    def deposita(self, valor):
+        self.__saldo += valor
 
-    def saca(self,valor):
-        self.saldo-=valor
+    def saca(self, valor):
+        self.__saldo -= valor
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
+
 
 conta = Conta(10, "Gabriela", 70)
 conta2 = Conta(11, "Julia", 90, 2000)
 
-print(conta.saldo)
+# conta.extrato()
+# conta.deposita(10)
+# conta.extrato()
+# conta.saca(20)
+# conta.extrato()
 
 conta.extrato()
-conta.deposita(10)
-conta.extrato()
-conta.saca(20)
-conta.extrato()
+conta2.extrato()
 
+conta.transfere(10, conta2)
 
+conta.extrato()
+conta2.extrato()
--- datas.py
+++ datas.py
@@ -3,9 +3,10 @@ class Data:
         self.dia = dia
         self.mes = mes
         self.ano = ano
-    
+
     def formatada(self):
         print("{}/{}/{}".format(self.dia, self.mes, self.ano))
 
-data = Data(17,7,200)
+
+data = Data(17, 7, 200)
 data.formatada()
